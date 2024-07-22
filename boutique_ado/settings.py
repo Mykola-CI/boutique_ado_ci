a2cc0b6f5e43f15b61de0740b8b91bdc0bfa4a72
Mykola Kuievda <dankraig23@gmail.com>
2024-07-22 17:03:53 +0100

    Correct settings for the variable DEVELOPMENT

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -220,7 +220,9 @@ STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY')
 STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET')
 
-if 'DEVELOPMENT' in os.environ:
+
+DEVELOPMENT = os.getenv('DEVELOPMENT')
+if DEVELOPMENT:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
     DEFAULT_FROM_EMAIL = 'boutiqueado@example.com'
 else:
