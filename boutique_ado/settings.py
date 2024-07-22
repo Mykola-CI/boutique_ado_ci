b60d8bb56c01af3f20702a2439256503ff99d428
Mykola Kuievda <dankraig23@gmail.com>
2024-07-22 17:33:09 +0100

    Correct env setting to os.getenv

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -228,6 +228,7 @@ if DEVELOPMENT:
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_USE_TLS = True
+    EMAIL_USE_SSL = False
     EMAIL_PORT = 587
     EMAIL_HOST = 'smtp.gmail.com'
     EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
